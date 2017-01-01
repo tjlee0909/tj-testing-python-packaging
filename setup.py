7693196d3adcfb080b0b7c3ae8f5ee5705bc4894
Taejun Lee <tj@box.com>
2016-12-31 17:19:17 -0800

    Add release url.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def main():
 		author='TJ Lee',
 		author_email='tj@box.com',
 		packages=['tjtestingpythonpackaging'],
-		download_url='',
+		download_url='https://github.com/tjlee0909/tj-testing-python-packaging/releases/tag/v0.1.0',
 		install_requires=[
 			'requests == 2.0',
 		],
