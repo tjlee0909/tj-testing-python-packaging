1c75cd155ffd1b20a6b5cc480e4591b0415b69f8
Taejun Lee <tj@box.com>
2016-12-31 17:20:47 -0800

    Bumped to version 0.1.1

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from distutils.core import setup
 def main():
 	setup(
 		name='tjtestingpythonpackaging',
-		version='0.1.0',
+		version='0.1.1',
 		description='TJ just testing python packaging',
 		author='TJ Lee',
 		author_email='tj@box.com',
 		packages=['tjtestingpythonpackaging'],
-		download_url='https://github.com/tjlee0909/tj-testing-python-packaging/releases/tag/v0.1.0',
+		download_url='https://github.com/tjlee0909/tj-testing-python-packaging/releases/tag/v0.1.1',
 		install_requires=[
 			'requests == 2.0',
 		],
